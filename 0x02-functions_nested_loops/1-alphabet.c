9040c6f060b4338f194fb5b06bb9e858972fe517
M1k1yas <mikiyas.kibret05@gmail.com>
2021-06-22 11:56:53 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_alhabet - prints alphabetin lowercase.
+ *
+ *Return: Void.
+ */
+void print_alhabet(void)
+{
+char i
+for (i = a; i <= z ; i++)
+{
+_putchar(i);
+i++;
+}
+
+_putchar("/n");
+}
