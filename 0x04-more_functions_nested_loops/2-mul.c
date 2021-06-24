face10d69d3229d652e09bf6e020777b2b648a16
M1k1yas <mikiyas.kibret05@gmail.com>
2021-06-24 04:13:17 -0700

    ul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - multiplies a and b
+ * @a: variable integer
+ * @b: variable integer
+ * Return: multiple of a and b
+ *
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
