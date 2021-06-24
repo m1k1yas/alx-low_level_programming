47964753c42f2e14deac4aea7465868cadd1ee0a
M1k1yas <mikiyas.kibret05@gmail.com>
2021-06-24 03:12:53 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_isupper - checks if uppercase
+ *@c: parameter integer
+ *Return: 1 if uppercase else 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
