c08b0187ba27c92d15f4183bed095b5a13862092
M1k1yas <mikiyas.kibret05@gmail.com>
2021-06-24 04:44:27 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints numbers
+ * Return: 0;
+ */
+
+void print_numbers(void)
+{
+int i = 48;
+
+while (i < 58)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
