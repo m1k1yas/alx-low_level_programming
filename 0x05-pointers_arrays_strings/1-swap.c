932ae3a0d418c8e11a22a27e982e81d048e3fc92
M1k1yas <mikiyas.kibret05@gmail.com>
2021-06-28 06:12:33 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the values of the variables
+ * @a: pointer variable
+ * @b: pointer variable
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
