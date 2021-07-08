cc7eceeb0f0107fc1a2f5317c306aaaa65862f70
M1k1yas <mikiyas.kibret05@gmail.com>
2021-07-08 11:48:51 -0700

    strlen recursive

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer parameter
+ * Return: count size
+ */
+int _strlen_recursion(char *s)
+{
+int i = 0;
+if(*s)
+{
+i++;
+return (i + _strlen_recursion(s + 1));
+}
+else
+{
+return (i);
+}
+}
