a8a950c05e21d00b3adf6d638669d6e694d88eb8
M1k1yas <mikiyas.kibret05@gmail.com>
2021-07-20 03:42:07 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,13 @@
-#ifndef struct
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - holds name, age, owner
+ *@name: First member
+ *@age: Second member
+ *@owner:Third member
+ *
+ * Description:
+ */
 struct dog
 {
 char *name;
