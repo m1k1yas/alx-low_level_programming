aa68f43699e5bec8710024b0e402a1b755a57559
M1k1yas <mikiyas.kibret05@gmail.com>
2023-03-16 09:35:36 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char string[] = "\"Programming is like building a multilingual puzzle";
+puts(string);
+return (0);
+}
