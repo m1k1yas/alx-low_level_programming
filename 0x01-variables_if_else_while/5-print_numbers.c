13a42cfae3518a68c7ab8ba4007fcbf31bb512fe
M1k1yas <mikiyas.kibret05@gmail.com>
2023-03-17 12:07:36 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+        printf("%i", i);
+}
+putchar(10);
+return (0);
+}
