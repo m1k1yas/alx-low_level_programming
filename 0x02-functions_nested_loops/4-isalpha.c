b4d23fbb5442e29ad2cf764d2abefd04f3d520e7
M1k1yas <mikiyas.kibret05@gmail.com>
2023-03-21 11:26:42 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
+ * _isalpha - checks for alphabetic character
+ * @c: value passed from the main function
  *
- *
- *
- *
+ * Return: 1 if the character is alphabetic, else 0
  */
 int _isalpha(int c)
 {
-	if((c > 96 && c < 123) || (c > 64 && c < 91))
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
 	{
 		return (1);
 	}
