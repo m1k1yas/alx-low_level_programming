4df7a5f67aba5ba9e7307fab0fea602e593d2245
M1k1yas <mikiyas.kibret05@gmail.com>
2023-03-30 11:31:57 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
