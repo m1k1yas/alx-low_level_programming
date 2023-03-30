081adb40a1878273b8cd1e84e270da624228ca10
M1k1yas <mikiyas.kibret05@gmail.com>
2023-03-30 09:39:09 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: returns the concatenated string
  *
  */
 char *_strcat(char *dest, char *src)
 {
-	int i=0, j=0;
+	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 	{
