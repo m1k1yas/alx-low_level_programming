c3f314dbc75bdc34123bf8033151a8595869cf20
M1k1yas <mikiyas.kibret05@gmail.com>
2023-03-30 12:05:36 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to be converted
+ * Return: returns the converted string
+ *
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+			}
+			continue;
+		}
+		if (str[i] == ' ')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+	}
+	return (str);
+}
