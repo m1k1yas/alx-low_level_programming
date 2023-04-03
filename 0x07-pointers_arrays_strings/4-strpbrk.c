87d385eccc963e107d240b931c4ddf237ec05baf
M1k1yas <mikiyas.kibret05@gmail.com>
2023-04-03 13:24:22 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strbrk - searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any of a set of bytes
  * @s: String to look for
  * @accept: String to look from
  *
