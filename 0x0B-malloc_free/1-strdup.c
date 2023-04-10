699f1376051166d5f9cc9a6b900378db8d563706
M1k1yas <mikiyas.kibret05@gmail.com>
2023-04-10 13:58:50 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	int i, r = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	arr = malloc(sizeof(char) * (i + 1));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (r = 0; str[r]; r++)
+	{
+		arr[r] = str[r];
+	}
+	return (arr);
+}
